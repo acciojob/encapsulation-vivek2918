d1f9ca20c402e3901a7a9869cb677d877b7716e1
unknown <chovatiyavivek1999@gmail.com>
2023-01-13 15:58:14 +0530

    Solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly p1 = new RWOnly();
+        //p1.setMember("Vivek");
+        // Error : Cannot Resolve the method 'setMember' in 'RWOnly'.
+
+        p1.setMember("Vivek");
+        System.out.println(p1.getMember());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String member;
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
 }
